5470c95724ee86c8c9b812c1ef27f19b17d4c528
Abhishek Raj <abhishekraj16574@gmail.com>
2026-04-20 12:05:57 +0530

    fix frontend path

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -6,42 +6,42 @@ import userRoute from "./Route/user.js";
 import noteRoute from "./Route/Todo.js";
 import cors from "cors";
 import path from "path";
-import { fileURLToPath } from "url";
 
 dotenv.config();
-const PORT=process.env.PORT || 4011;
-const URI=process.env.MONGO_URI;
+
+const PORT = process.env.PORT || 4011;
+const URI = process.env.MONGO_URI;
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(cors({
   origin: true,
   credentials: true
 }));
 
 mongoose
-.connect(URI)
-.then(()=>console.log("MongoDb is connected"))
-.catch((error)=>console.log("Mongo nDB is not connected"));
+  .connect(URI)
+  .then(() => console.log("MongoDb is connected"))
+  .catch((error) => console.log("MongoDB not connected", error));
 
-
-
-app.get("/ping",(req,res)=>{
+app.get("/ping", (req, res) => {
   res.send("PONG");
 });
 
-app.use("/api/v1/user",userRoute);
-app.use("/api/v1/note",noteRoute);
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/note", noteRoute);
 
 const __dirname = path.resolve();
+
 app.use(express.static(path.join(__dirname, "Frontend/dist")));
 
-app.get("/*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "Frontend/dist/index.html"));
 });
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
-})
+});
